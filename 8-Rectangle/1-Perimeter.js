213531f5d7f01f0d5cac20a6e01ef42fa8fa920b
saharoori <saharlaor@gmail.com>
2021-11-13 22:58:28 +0200

    8.1 - Perimeter Of Rectangle

---
+++ 8-Rectangle/1-Perimeter.js
@@ -0,0 +1,7 @@
+function perimeter(length, width) {
+    return (length + width) * 2;
+}
+
+console.log(perimeter(6, 7)); // 26
+console.log(perimeter(20, 10)); // 60
+console.log(perimeter(2, 9)); // 22
