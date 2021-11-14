33753f62842d83b364be23fcaf39eb49dc891a1c
saharoori <saharlaor@gmail.com>
2021-11-14 09:45:51 +0200

    5.8 - All longest words

--- 5-Iteration/8-longest_words.js
+++ 5-Iteration/8-longest_words.js
@@ -1,8 +1,15 @@
 function longestWordLength(sentence) {
-    return sentence
+    let wordLengthObj = sentence
         .split(" ")
         .map((word) => [word.length, word])
-        .sort((a, b) => b[0] - a[0])[0][1];
+        .sort((a, b) => b[0] - a[0]);
+    const maxLength = wordLengthObj[0][0];
+    return wordLengthObj
+        .filter((word) => word[0] === maxLength)
+        .map((word) => word[1])
+        .join(", ");
 }
 
-console.log(longestWordLength("asdfd ret afda sahar laor pini hodadad"));
+console.log(
+    longestWordLength("asdfd ret afda sahar laor pini hodadad asdfghj")
+);
